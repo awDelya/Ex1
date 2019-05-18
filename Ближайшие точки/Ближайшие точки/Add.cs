a871afa2aa4b51023f0b37ddc9a4012e6530b784
Delya <deley@mail.ru>
2019-05-18 20:36:34 +0500

    Add function for opening files

--- Ближайшие точки/Ближайшие точки/Add.cs
+++ Ближайшие точки/Ближайшие точки/Add.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,20 +13,56 @@ namespace Ближайшие_точки
 {
     public partial class Add : Form
     {
+        public List<int> ListPoint = new List<int>();//для записи точек
+        public string WhereFile = "";
         public Add()//главная форма
         {
             InitializeComponent();
-            AddNumber.MaxLength = 6;
+            AddNumber.MaxLength = 6;//ограничения на кол-во символов в числе
+            openFileDialog1.Filter = "File Input(*.txt)|*.txt";//расширение для файла
         }
 
         private void AddOldData_Click(object sender, EventArgs e)//добавление существующих точек из файлов
         {
-
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;//если нажали в окне открытия файла "Отмена"
+            WhereFile = openFileDialog1.FileName;//запись пути выбранного файла
+            using (StreamReader file = new StreamReader(WhereFile))//открытие выбранного файла для чтения
+            {
+                string temp = file.ReadLine();//чтение строки
+                if (temp == null) //проверка на пустоту
+                    { MessageBox.Show("Данные файла не подходят под условия задачи."); return; }
+                try
+                {
+                    int count = Convert.ToInt32(temp);
+                    if (count >= 2 && count <= 100000)
+                        AddNumber.Text = temp;//добавление данных в форму
+                    else
+                    { AddNumber.Text = ""; AddPoints.Text = ""; MessageBox.Show("Данные файла не подходят под условия задачи."); return; }
+                    temp = file.ReadLine();//строка файла открывается на чтение
+                    if (temp == null)//проверка на пустоту
+                    { AddNumber.Text = ""; AddPoints.Text = ""; MessageBox.Show("Данные файла не подходят под условия задачи."); return; }
+                    else
+                    {
+                        var array = temp.Split(' ');//запись данных
+                        for(int i=0;i<array.Length; i++)//перебор элементов для добавлеия в лист
+                        {
+                            int x = Convert.ToInt32(array[i]);
+                            ListPoint.Add(x);
+                        }
+                    }
+                    AddPoints.Text = temp;//вывод в форму
+                    Res.Enabled = true;
+                }
+                catch
+                { AddNumber.Text = ""; AddPoints.Text = ""; MessageBox.Show("Данные файла не подходят под условия задачи."); }
+               
+            }
         }
 
         private void Res_Click(object sender, EventArgs e)//кнопка для показа результата
         {
-
+            Result forRes = new Result(this);
+            forRes.ShowDialog();
         }
 
         private void AddNumber_KeyPress(object sender, KeyPressEventArgs e)//нажатие клавиши
--- Ближайшие точки/Ближайшие точки/Result.cs
+++ Ближайшие точки/Ближайшие точки/Result.cs
@@ -6,20 +6,28 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Ближайшие_точки
 {
     public partial class Result : Form
     {
-        public Result()
+        Add add;
+        public string WhereFile = "";//путь для хранения файла
+
+        public Result(Add znach)
         {
+            add = znach;
             InitializeComponent();
         }
 
         private void SavePoints_Click(object sender, EventArgs e)//сохранение введенных точек в файл
         {
+            using (StreamReader file = new StreamReader(WhereFile))//открытие выбранного файла для чтения
+            {
 
+            }
         }
     }
 }
